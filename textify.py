10f54db6ff9d8b32e3f96616003ec85ca857b098
Wojtek <drlech@gmail.com>
2018-01-28 15:58:27 +0100

    Add comments

--- textify.py
+++ textify.py
@@ -3,6 +3,16 @@ import sys
 from pathlib import Path
 
 def get_luminance(rgb):
+    """
+    Get the normalized (0-1) luminance based on RBG values of the color.
+
+    Args:
+        rgb (tuple): RGB colors to find luminance for.
+
+    Returns:
+        int: Pixel luminence.
+    """
+
     r = rgb[0]
     g = rgb[1]
     b = rgb[2]
@@ -13,18 +23,30 @@ def get_luminance(rgb):
     return luminance / 255;
 
 def filter_file(filename):
+    """
+    Process the image file.
+
+    Args:
+        filename (string): Name of the file to process.
+    """
+
     image = Image.open(filename)
 
     image_size = image.size
     image_width = image_size[0]
     image_height = image_size[1]
 
+    # Output file
     output = Image.new('RGB', (image_width, image_height), (255, 255, 255))
     output_pixels = output.load()
 
     # Convert to RGB just in case file is in different format?
     rgb_image = image.convert('RGB')
 
+    # Convert each pixel to either black or white color.
+    # That is based on pixel luminance (normalized to 0-1). We sum the luminance of the pixels
+    # as we go, as long as it's below 1 we print a white pixel, when it goes over one
+    # we print a black pixel.
     brightness = 0
     for h in range(image_height):
         for w in range(image_width):
@@ -36,9 +58,17 @@ def filter_file(filename):
 
             output_pixels[w, h] = color
 
+    # Save output file
     output.save('output.png', 'PNG')
 
 def open_and_filter(filename):
+    """
+    Check if file exists, and if it does - run the filter.
+
+    Args:
+        filename (string): Name of the file to process.
+    """
+
     file = Path(filename)
     if (file.is_file()):
         filter_file(filename)
@@ -46,6 +76,10 @@ def open_and_filter(filename):
         print('Given file does not exist.')
 
 def main():
+    """
+    Input file should be passed as a first argument.
+    """
+
     if len(sys.argv) > 1:
         open_and_filter(sys.argv[1])
     else:
